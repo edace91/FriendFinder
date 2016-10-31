cebb46ed2011d03153a6334516d21926ac1e3a1e
edace91 <eduace91@gmail.com>
2016-10-31 13:02:29 -0500

    added htm example and instructions in code

---
+++ app/routing/api-routes.js
@@ -0,0 +1,20 @@
+//A GET Route to /survey which 
+//should display the survey page.
+
+
+
+
+
+
+
+// =============================================================
+//A default USE route that leads to home.html 
+//which displays the home page.
+
+
+
+
+
+
+
+// =============================================================
